7e8075353f555ea9cf0662223f21ed4db5bb1e46
mgrecu35 <mirgrecu@gmail.com>
2025-03-21 15:35:48 -0400

    bug fixes

--- onnx_gen_interface.c
+++ onnx_gen_interface.c
@@ -10,16 +10,17 @@
 typedef struct {
     char name[50];
     int64_t shape[4];
-    size_t shape_len;
+    int shape_len;
+    int n_elem;
     ONNXTensorElementDataType dtype;
 } TensorInfo;
 
 typedef struct {
     char model_path[150];
     TensorInfo inputs[MAX_INPUTS];
-    size_t num_inputs;
+    int num_inputs;
     TensorInfo outputs[MAX_OUTPUTS];
-    size_t num_outputs;
+    int num_outputs;
     float *input_data[MAX_INPUTS];
     float *output_data[MAX_OUTPUTS];
 } ModelInfo;
@@ -36,6 +37,27 @@ OrtMemoryInfo* memory_info;
 OrtStatus *status;
 OrtApi *g_api;
 
+void set_input_data_(int *model_index, int *input_index, float *data) {
+    ModelInfo *model = &models[*model_index];
+    int i;
+    for (i = 0; i < model->inputs[*input_index].n_elem; i++) {
+        model->input_data[*input_index][i] = data[i];
+    }
+}
+void get_output_data_(int *model_index, int *output_index, float *data) {
+    ModelInfo *model = &models[*model_index];
+    int i;
+    //printf("output index %d\n", *output_index);
+    //printf("output n_elem %d\n", model->outputs[*output_index].n_elem);
+    if (model->output_data[*output_index] == NULL) {
+        printf("Error: output_data[%d] is NULL!\n", *output_index);
+        return;
+    }
+    //return;
+    for (i = 0; i < model->outputs[*output_index].n_elem; i++) {
+        data[i]=model->output_data[*output_index][i];
+    }
+}
 void init_onnx_runtime_(void) {
     // Initialize the ONNX Runtime environment
     g_api = OrtGetApiBase()->GetApi(ORT_API_VERSION);
@@ -58,39 +80,66 @@ void init_onnx_runtime_(void) {
 
 void call_onnx_(int *model_index) {
     ModelInfo *model = &models[*model_index];
-    const char* input_names[MAX_INPUTS];
-    const char* output_names[MAX_OUTPUTS];
+    char* input_names[MAX_INPUTS];
+    char* output_names[MAX_OUTPUTS];
     const OrtValue* input_tensors[MAX_INPUTS];
     OrtValue* output_tensors[MAX_OUTPUTS];
-
+    //printf("model num_inputs %d\n",model->num_inputs);
+    //printf("model num_outputs %d\n",model->num_outputs);
+    for (size_t i = 0; i < model->num_inputs; i++) {
+        input_names[i] = (char *)malloc(50 * sizeof(char));
+    }
+    for (size_t i = 0; i < model->num_outputs; i++) {
+        output_names[i] = (char *)malloc(50 * sizeof(char));
+    }
     // Prepare input tensors
     for (size_t i = 0; i < model->num_inputs; i++) {
-        input_names[i] = model->inputs[i].name;
-        int64_t *shape = model->inputs[i].shape;
+        strcpy(input_names[i], model->inputs[i].name);
+        int64_t *shape = (int64_t *)malloc(model->inputs[i].shape_len * sizeof(int64_t));
+        for (size_t j = 0; j < model->inputs[i].shape_len; j++) {
+            shape[j] = model->inputs[i].shape[j];
+        }
         size_t shape_len = model->inputs[i].shape_len;
         size_t tensor_size = 1;
         for (size_t j = 0; j < shape_len; j++) {
             tensor_size *= shape[j];
         }
         status = g_api->CreateTensorWithDataAsOrtValue(
-            memory_info, model->input_data, tensor_size * sizeof(float),
+            memory_info, model->input_data[i], tensor_size * sizeof(float),
             shape, shape_len, model->inputs[i].dtype, &input_tensors[i]);
+        if (status != NULL) {
+                const char* msg = g_api->GetErrorMessage(status);
+                fprintf(stderr, "Failed to create input tensor: %s\n", msg);
+                g_api->ReleaseStatus(status);}
     }
 
     // Prepare output tensors
     for (size_t i = 0; i < model->num_outputs; i++) {
-        output_names[i] = model->outputs[i].name;
-        int64_t *shape = model->outputs[i].shape;
+        strcpy(output_names[i],model->outputs[i].name);
+        int64_t *shape = (int64_t *)malloc(model->outputs[i].shape_len * sizeof(int64_t));
+        for (size_t j = 0; j < model->outputs[i].shape_len; j++) {
+            shape[j] = model->outputs[i].shape[j];
+        }
+        //model->outputs[i].shape;
         size_t shape_len = model->outputs[i].shape_len;
         size_t tensor_size = 1;
         for (size_t j = 0; j < shape_len; j++) {
             tensor_size *= shape[j];
         }
         status = g_api->CreateTensorWithDataAsOrtValue(
-            memory_info, model->output_data, tensor_size * sizeof(float),
+            memory_info, model->output_data[i], tensor_size * sizeof(float),
             shape, shape_len, model->outputs[i].dtype, &output_tensors[i]);
+        if (status != NULL) {
+                const char* msg = g_api->GetErrorMessage(status);
+                fprintf(stderr, "Failed to create output tensor: %s\n", msg);
+                g_api->ReleaseStatus(status);}
     }
-
+    //printf("got here\n");
+    //printf("model index: %d\n", *model_index);
+    //printf("input name %s\n", input_names[0]);
+    //printf("output name %s\n", output_names[0]);
+    //printf("%d\n", model->num_inputs);
+    //printf("%d\n", model->num_outputs);
     // Run the model
     status = g_api->Run(
         sessions[*model_index], NULL, input_names, input_tensors, model->num_inputs,
@@ -101,12 +150,18 @@ void call_onnx_(int *model_index) {
         fprintf(stderr, "Failed to run ONNX model: %s\n", msg);
         g_api->ReleaseStatus(status);
     }
-
+    
     // Release resources
     for (size_t i = 0; i < model->num_inputs; i++) {
         g_api->ReleaseValue(input_tensors[i]);
     }
     for (size_t i = 0; i < model->num_outputs; i++) {
         g_api->ReleaseValue(output_tensors[i]);
+    }
+    for (size_t i = 0; i < model->num_inputs; i++) {
+        free(input_names[i]);
+    }
+    for (size_t i = 0; i < model->num_outputs; i++) {
+        free(output_names[i]);
     }
 }
---
+++ onnx_interface.f90
@@ -0,0 +1,79 @@
+
+
+subroutine init_onnx()
+    call models_details_c()
+    call init_onnx_runtime()
+end subroutine init_onnx
+
+!-- 0 GMI_ONNX_Models/dense_autoencoder_land.onnx {'inputs': [{'name': 'input', 'shape': [1, 11], 'dtype': 'FLOAT32'}], 'outputs': [{'name': 'output', 'shape': [1, 11], 'dtype': 'FLOAT32'}]} 
+!-- 1 GMI_ONNX_Models/dense_autoencoder_ocean.onnx {'inputs': [{'name': 'input', 'shape': [1, 11], 'dtype': 'FLOAT32'}], 'outputs': [{'name': 'output', 'shape': [1, 11], 'dtype': 'FLOAT32'}]} 
+
+subroutine read_scaler_data()
+    use scalers
+    open(10,file="GMI_ONNX_Models/scaler_land_qv.bin",form="unformatted",status="old")
+    read(10) scaler_ocean_qv%mean
+    read(10) scaler_ocean_qv%std
+    close(10)
+    open(10,file="GMI_ONNX_Models/scaler_ocean_qv.bin",form="unformatted",status="old")
+    read(10) scaler_land_qv%mean
+    read(10) scaler_land_qv%std
+    close(10)
+end subroutine read_scaler_data
+
+subroutine call_dense_qv(tskin,qv,isurf,x_output)
+    use scalers
+    implicit none
+    real :: qv(10), tskin
+    integer :: isurf
+    real :: x_input(11)
+    integer :: model_index, input_index, output_index
+    real,intent(out) :: x_output(4)
+    integer :: i
+    !integer :: model_index, input_index
+    !print*, "tskin", tskin
+    !print*, "qv", qv
+    !print*, "isurf", isurf
+    !print*, "scaler_land_qv%mean", scaler_land_qv%mean
+    !print*, "scaler_land_qv%std", scaler_land_qv%std
+    !print*, "scaler_ocean_qv%mean", scaler_ocean_qv%mean
+    !print*, "scaler_ocean_qv%std", scaler_ocean_qv%std
+    
+    if(isurf==1) then
+        x_input(1)=(tskin-scaler_land_qv%mean(1))/scaler_land_qv%std(1)
+        do i=1,10
+            x_input(i+1)=(qv(i)-scaler_land_qv%mean(i+1))/scaler_land_qv%std(i+1)
+        end do
+    else
+        x_input(1)=(tskin-scaler_ocean_qv%mean(1))/scaler_ocean_qv%std(1)
+        do i=1,10
+            x_input(i+1)=(qv(i)-scaler_ocean_qv%mean(i+1))/scaler_ocean_qv%std(i+1)
+        end do
+    end if
+    if (isurf==1) then
+        model_index=4
+    else
+        model_index=5
+    end if
+    !print*, "x_input", x_input
+    
+    input_index=0
+    output_index=0
+    call set_input_data(model_index, input_index, x_input)
+    !print*, "out of set_input_data"
+    call call_onnx(model_index)
+    !print*, input_index
+    call get_output_data(model_index, output_index, x_output)
+    !print*, "x_output", x_output
+    return
+end subroutine call_dense_qv
+
+subroutine set_input_data_unet(imodel, input_data, output_data, &
+    ichan_in, ichan_out, n_x, n_y)
+    integer, intent(in) :: imodel, ichan_in, n_x, n_y
+    integer, intent(in) :: ichan_out
+    real, intent(in) :: input_data(ichan_in, n_x, n_y)
+    real, intent(out) :: output_data(ichan_out, n_x, n_y)
+
+    
+end subroutine set_input_data_unet
+
